4f0e1599317a7b9a754f0f4956c276145df7451c
mdarsa <mdarsa@murex.com>
2024-01-10 10:12:35 +0200

    Refactor code and optimize effects in App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import "./App.css";
 import {
-  Bloom,
   BrightnessContrast,
   DepthOfField,
   EffectComposer,
   FXAA,
   Noise,
-  SMAA,
   Vignette,
 } from "@react-three/postprocessing";
-import { BlurPass, Resizer, KernelSize } from "postprocessing";
 import {
-  CubeCamera,
   Environment,
+  Loader,
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
@@ -28,14 +25,9 @@ function App() {
       <Environment files="./moonless_golf_2k.hdr" />
       <color attach="background" args={["#000"]} />
       //create a camera as default camera
-      <PerspectiveCamera fov={45} position={[0, 1.5, -5]} makeDefault />
+      <PerspectiveCamera fov={45} position={[0, 1, -4]} makeDefault />
       {/* orbit controls */}
-      <OrbitControls
-        target={[0, 0.25, 0]}
-        autoRotate
-        maxDistance={10}
-        minDistance={5}
-      />
+      <OrbitControls target={[0, 0.25, 0]} maxDistance={10} minDistance={5} />
       {/* scene */}
       <Scene />
       {/* spot light with helper */}
@@ -47,23 +39,21 @@ function App() {
         penumbra={1}
       />
       {/* effects */}
-      <Suspense>
-        <EffectComposer multisampling={0}>
-          <DepthOfField
-            focusDistance={0}
-            focalLength={0.05}
-            bokehScale={1}
-            height={480}
-          />
-          <FXAA />
-          <Noise opacity={0.03} />
-          <Vignette eskil={false} offset={0.1} darkness={1.2} />
-          <BrightnessContrast
-            brightness={0} // brightness. min: -1, max: 1
-            contrast={0.1} // contrast: min -1, max: 1
-          />
-        </EffectComposer>
-      </Suspense>
+      <EffectComposer multisampling={0}>
+        <FXAA />
+        <DepthOfField
+          focusDistance={0}
+          focalLength={0.05}
+          bokehScale={1}
+          height={480}
+        />
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.2} />
+        <BrightnessContrast
+          brightness={0} // brightness. min: -1, max: 1
+          contrast={0.1} // contrast: min -1, max: 1
+        />
+      </EffectComposer>
       <fog attach="fog" args={["#000", 5, 35]} />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
+import { Html, useProgress } from "@react-three/drei";
+
+function Loader() {
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return (
+    <>
+      <Html fullscreen>
+        <div className="black-screen">
+          <div className="loading-bar">
+            <div
+              className="loading-bar-inner-1"
+              style={{ transform: `scaleX(${progress / 100.0})` }}
+            ></div>
+          </div>
+        </div>
+      </Html>
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,7 +36,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       }}
       dpr={[1, 2]}
     >
-      <App />
+      <Suspense fallback={<Loader />}>
+        <App />
+      </Suspense>
     </Canvas>
   </React.StrictMode>
 );
